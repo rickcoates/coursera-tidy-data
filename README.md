641d20470599469482bbfa8489cc16bebef9b087
Rick Coates <rick@jacketscreative.com>
2014-05-23 11:10:21 -0300

    Added comments to the R script file and completed the first draft of the README.

--- README.md
+++ README.md
@@ -1,4 +1,116 @@
-coursera-tidy-data
-==================
+### Introduction
 
-Repo for Coursera Data Science Specialization Getting and Cleaning Data Course Project
+The functions in the run_analysis.R file implement a solution to the Course Project for the Coursera Data Science Specialization Getting and Cleaning Data MOOC.
+
+The stated purpose of the project/assignment is to "demonstrate an ability to collect, work with, and clean a data set, with the ultimate goal of preparing a tidy data that can be used for later analysis."
+
+The "unclean" data to be "tidied" is a data set collected from the accelerometers in Samsung Galaxy S smartphones, known as the "Human Activity Recognition (HAR) Using Smartphones Dataset Version 1.0." The raw/untidy data consist of both training and test data (that are to be combined). For the purpose of this analysis (and the specific requirements of the Course Project) the raw triaxial acceleration data from the smartphone's accelerometer (total acceleration), the estimated body  acceleration data, and the triaxial angular velocity from the smartphone's gyroscope (contained in *Inertial Signals* subdirectories of the raw HAR data set) are **not** considered. Rather, the tidy data set for later analysis is constructed from a 561-feature vector of time and frequency domain variables for each observation (*X_test.txt* and *X_train.txt* files), the activity number/label associated with the feature vector (*y_test.txt* and *y_train.txt* files) and the (numeric/anonymized) identifier of the human subject who carried the smartphone (*subject_test.txt* and *subject_train.txt* files). For details on the resulting (second, independent) tidy data set, see CodeBook.md.
+
+### Running the Analysis
+
+The main analysis, which creates the second tidy data set (as defined by the assignment), can be run by calling the `run_analysis` function.  
+
+	run_analysis <- function(download.data = F, directory = "UCI HAR Dataset")
+
+To download the raw HAR data from source, set `download.data = TRUE`. To change the root directory of where the data set resides (or where it should be placed if downloaded from source), set the `directory` parameter to the desired directory.
+
+A data.frame containing the second tidy data set, as defined in CodeBook.md, is returned. The same data set is also saved as a CSV file, named *HAR_Tidy_Summarized.csv*, to the working directory.
+
+If the first (unsummarized) tidy data set is of interest, it can be created independently as follows:
+
+	  despaced.test.filename  <- CleanTestOrTrainSpaces("test", directory)
+	  despaced.train.filename <- CleanTestOrTrainSpaces("train", directory)
+	  mean.and.stdev.measurements <- MeasurementsOfInterest(directory)
+	  tidy.data <- ReadDataAndCreateDataFrame(despaced.test.filename, 
+	  					despaced.train.filename, 
+	  					mean.and.stdev.measurements, 
+	  					directory)
+
+where the `directory` parameter is the root directory of the raw HAR data set.
+
+### Summary of the Analysis Performed
+
+#### Step 1
+
+Extraneous spaces are removed from the measurement data file for both the test and train data sets (*X_test.txt* and *X_train.txt*), so it is possible to (fast) read in measurement data using the fread function in the `data.table` package, *plus*,(simultaneously) subset the data set to the columns of interest (measurements with both mean and standard deviation statistics). 
+
+The result of this step are new "despaced" versions of the *X_test.txt* and *X_train.txt* files, named *despaced.X_test.txt* and *despaced.X_train.txt* respectively.
+
+The function that performs this step is:
+
+	CleanTestOrTrainSpaces <- function(test.or.train = "test", directory = "UCI HAR Dataset")
+
+which is run twice. Once for the test data set and once for the train data set.
+
+#### Step 2
+
+Determine the measurements to extract from the test and train data sets based on the names of the features given in the *features.txt* file that comes with the HAR data set.
+
+The function that performs this step is:
+
+	MeasurementsOfInterest <- function(directory = "UCI HAR Dataset") 
+
+The method for determining which features/measurements to extract is to `regex` on the measurement variable names (as defined in the HAR data set's *features.txt* file) and subset on names that contain `-mean()` or `-std()`. Note that this eliminates the `*-meanFreq()` measurements/variables on the assumption that, since there is no corresponding standard deviation measurement for the variable, the `*-meanFreq()` variables should be excluded, which satisfies Course Project requirement #2:
+
+* *Extracts only the measurements on the mean and standard deviation for each measurement.*
+
+#### Step 3
+
+1. Read in the "despaced" test and train data sets created in **Step 1**, but only the columns containing features with both mean and standard deviation measurements, as determined in **Step 2**.
+2. Clean up the measurement variable names so they are meaningful/readable.
+3. Add in the activity label column (with meaning activity label names rather than the activity label *numbers* in the raw *y_test.txt* and *y_train.txt* files) 
+4. Add in the subject id column
+5. Merge the test and train data sets.
+
+The function that performs this step is:
+
+	ReadDataAndCreateDataFrame <- function(test.measurements, train.measurements, mean.and.stdev.measurements, directory = "UCI HAR Dataset") 
+ 
+This step satisfies Course Project requirements #1, #2 (in combination with **Step 2**), #3 and #4:
+
+* Extracts only the measurements on the mean and standard deviation for each measurement. 
+* Uses descriptive activity names to name the activities in the data set
+* Appropriately labels the data set with descriptive activity names. 
+
+#### Step 4
+
+Create a second, independent tidy data set  that summarizes all of the measurement variables in the first tidy data set (created in **Steps 1 - 3**) *by activity (label) and subject*. The name of the summarized (average/mean) version of the each measurement variable is set to be name of the original variable with `.avg` appended. The result contains 180 observations (30 subjects * 6 different activities) of the 66 summarized measurements of interest (in contrast to the 10,299 total observations for the 66 measurements of interest in the first tidy data set). For more details on the second (final result) tidy data set, see CodeBook.md. 
+
+The function that performs this step is:
+
+	CreateSecondTidyDataSet <- function(data) 
+ 
+This step satisfies Course Project requirements #5:
+
+* Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+ 
+### Complete List of Analysis Functions
+
+**ActivityLabelMapping** - Reads in the activity labels associated with the activity numbers in the HAR data set (from the activity_labels.txt file).
+
+**AddActivityLabels** - Adds activity labels to either a test or train set of measurement observations.
+
+**AddSubjectIdentifers** - Adds subject identifiers to either a test or train set of measurement observations.
+
+**CleanTestOrTrainSpaces** -   Removes extraneous spaces from the feature vector data for either the test or train data set. This function takes advantage of the format of the data being the same for both the test and train data sets.
+
+**CreateSecondTidyDataSet** - Creates a second tidy data set that contains summary statistics of first tidy data set.
+
+**MeasurementsOfInterest** -   Extracts the mean and standard dev measurement column numbers and variable names from the HAR data set features.txt file. The measurements to extract are determined from the measurement variable name and are limited to measurements for which there is BOTH a mean and standard deviation.
+
+**ReadDataAndCreateDataFrame** -   Reads in mean and standard deviation variables from the HAR feature vector data, adds the subject identifiers and activity labels for both the test and train data sets and merges them.
+
+**ReadInDataAndExtractMeasurements** -   Reads in a (despaced) raw HAR feature vector data file, subsets it by a vector of column numbers and assigns meaningful names to the measurement variables based on a vector of raw measurement names (with "problematic" characters removed or replaced).
+
+**run_analysis** - Runs the complete analysis, including (optionally) downloading the data set from source.
+
+### Versions and Packages Used
+
+**R Version:** 3.0.3 (2014-03-06) a.k.a Warm Puppy
+
+####Packages:
+* plyr 1.8.1
+* data.table 1.9.1
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,35 @@
+## These following functions implement a solution to the Course Project for the
+## Coursera Data Science Specialization Getting and Cleaning Data MOOC.
+## 
+## The stated purpose of the project/assignment is to demonstrate an ability to collect, 
+## work with, and clean a data set, with the ultimate goal of preparing a tidy data that 
+## can be used for later analysis.
+##
+## The "unclean" data to be "tidied" is a data set collected from the accelerometers in
+## Samsung Galaxy S smartphones known as the "Human Activity Recognition Using Smartphones 
+## Dataset Version 1.0." The raw/untidy data consist of both training and test data (that 
+## are to be combined). For the purpose of this analysis, the raw triaxial acceleration 
+## data from the smartphone's accelerometer (total acceleration), the estimated body 
+## acceleration data, and the triaxial angular velocity from the smartphone's gyroscope
+## are NOT considered. Rather, the tidy data set for later analysis is constructed from
+## a 561-feature vector of time and frequency domain variables for each observation, the 
+## activity label associated with the feature vector (one of six: WALKING, WALKING_UPSTAIRS, 
+## WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) and the (numeric/anonymized) identifier 
+## of the human subject who carried the smartphone.
+
 run_analysis <- function(download.data = F, directory = "UCI HAR Dataset") {
+  ## Runs the complete analysis, including (optionally) downloading the data set 
+  ## from source.
+  ##
+  ## Args: 
+  ##   download.data: If TRUE, download the data set from source, Default is FALSE.
+  ##   directory: The root directory of where the data set resides or should be 
+  ##   placed if it is downloaded from source.
+  ##
+  ## Returns:
+  ##   A tidy data set as defined in CodeBook.md. The data set is also saved as a 
+  ##   CSV to the working directory.
+
   ## download the data set and extract the zip file if requested
   if (download.data == T) {
     message("downloading the data set...")
@@ -19,20 +50,44 @@ run_analysis <- function(download.data = F, directory = "UCI HAR Dataset") {
   }
   ## first, remove extraneous spaces from the measurement data file so it is possible
   ## to read in the measurement data using the fread function in the data.table package
+  message("performing initial cleaning of the feature vector files...")
   despaced.test.filename  <- CleanTestOrTrainSpaces("test", directory)
   despaced.train.filename <- CleanTestOrTrainSpaces("train", directory)
   ## determine the measurements to extract from the test and train data sets based on
   ## the names of the features given in the features.txt file that comes with the data set
   message("determining features with both mean and stdev measurements...")
   mean.and.stdev.measurements <- MeasurementsOfInterest(directory)
-  ## read in the data sets and only the features with both mean and stdev measurements
-  ## merge the test and train data sets
+  ## read in the data sets and only the features with both mean and stdev measurements,
+  ## add in the activity labels and subject columns, tidy everything up and, finally, 
+  ## merge the test and train data sets.
+  message("reading in the various data set files and constructing a clean data frame...")
   tidy.data <- ReadDataAndCreateDataFrame(despaced.test.filename, despaced.train.filename, mean.and.stdev.measurements, directory)
-  tidy.data
+  message("creating the second tidy data set (summarizing the first tidy data set)...")
+  second.tidy.data.set <- CreateSecondTidyDataSet(tidy.data)
+  message("writing out the second tidy data set to the working directory...")
+  write.csv(second.tidy.data.set,"HAR_Tidy_Summarized.csv")
+  second.tidy.data.set
 }
 
 CreateSecondTidyDataSet <- function(data) {
+  ## Creates a second tidy data set that contains summary statistics of first 
+  ## tidy data set.
+  ##
+  ## Args: 
+  ##   download.data: The first tidy data set, a data frame containing all of
+  ##                  the measurement variables of interest for each observation 
+  ##                  as well as the activity label associated with the observation 
+  ##                  and the subject identifier.
+  ##
+  ## Returns:
+  ##   A second, independent tidy data set with the average of each variable 
+  ##   for each activity and each subject. This data set as defined in CodeBook.md.
+  
   require(plyr)
+  ## use ddply to summarize all of the measurement variables of interest by
+  ## activity (label) and subject. The name of the summarized (average/mean) 
+  ## version of the variable is set to be name of the original variable with 
+  ## .avg appended.
   tidy2.df <- ddply(data, .(ActivityLabel, SubjectNumber), summarize, 
                             tBodyAcc.mean.X.avg           = mean(tBodyAcc.mean.X),
                             tBodyAcc.mean.Y.avg           = mean(tBodyAcc.mean.Y),         
@@ -104,6 +159,19 @@ CreateSecondTidyDataSet <- function(data) {
 }
 
 CleanTestOrTrainSpaces <- function(test.or.train = "test", directory = "UCI HAR Dataset") {
+  ## Removes extraneous spaces from the feature vector data for either the test or
+  ## train data set. This function takes advantage of the format of the data being the same 
+  ## for both the test and train data sets.
+  ##
+  ## Args: 
+  ##   test.or.train: A character variable that indicates which raw data set to process
+  ##                  (the test set or the train set)
+  ##   directory: The root directory of where the raw HAR data set resides. 
+  ##
+  ## Returns:
+  ##   The filename of the (new) "despaced" version of either the test or train raw
+  ##   feature vector data.
+  
   if (!(test.or.train %in% c("test", "train"))) {
     stop("invalid data subset requested; it must be either 'test' or 'train'")
   }
@@ -127,6 +195,20 @@ CleanTestOrTrainSpaces <- function(test.or.train = "test", directory = "UCI HAR 
 }
 
 ReadDataAndCreateDataFrame <- function(test.measurements, train.measurements, mean.and.stdev.measurements, directory = "UCI HAR Dataset") {
+  ## Reads in mean and standard deviation variables from the HAR feature vector 
+  ## data, adds the subject identifiers and activity labels for both the test 
+  ## and train data sets and merges them.
+  ##
+  ## Args:
+  ##   test.measurements: The filename of the despaced version of the test feature vector data.
+  ##   traing.measurements: The filename of the despaced version of the train feature vector data.
+  ##   mean.and.stdev.measurements: A vector of the columns (variables) of interest 
+  ##                                subsetted from the complete 561-feature vector.
+  ##   directory: The root directory of where the raw HAR data set resides. 
+  ##
+  ## Returns:
+  ##   A data frame of the cleaned and merged test and train data sets.
+  
   message("reading in the mean and stdev measurements from the test data set...")
   test.df  <- ReadInDataAndExtractMeasurements(test.measurements, mean.and.stdev.measurements)
   message("adding subject id's and activity labels to test data set...")
@@ -144,6 +226,23 @@ ReadDataAndCreateDataFrame <- function(test.measurements, train.measurements, me
 }
 
 ReadInDataAndExtractMeasurements <- function(filename, means.and.stdevs) {
+  ## Reads in a (despaced) raw HAR feature vector data file, subsets it by a vector
+  ## of column numbers and assigns meaningful names to the measurement variables
+  ## based on a vector of raw measurement names (with "problematic" characters
+  ## removed or replaced).
+  ##
+  ## Args:
+  ##   filename: The filename of the despaced version of the feature vector data.
+  ##   means.and.stdevs: A data frame of two variables. The first column is the
+  ##                     column number of either a mean or standard deviation
+  ##                     measurement from the full  561-feature vector and the second
+  ##                     column is the measurement variable's name from the HAR data 
+  ##                     set's feature.txt file.
+  ##
+  ## Returns:
+  ##   A data frame containing one row for each measurement and a column for each
+  ##   measurement that is either a mean or a standard deviation measurement.
+  
   require(data.table)
   ## (fast) read in a data.table with only the measurements on the mean and standard 
   ## deviation for each measurement.
@@ -158,6 +257,19 @@ ReadInDataAndExtractMeasurements <- function(filename, means.and.stdevs) {
 }
 
 MeasurementsOfInterest <- function(directory = "UCI HAR Dataset") {
+  ## Extracts the mean and standard dev measurement column numbers and variable
+  ## names from the HAR data set features.txt file. The measurements to extract
+  ## are determined from the measurement variable name and are limited to 
+  ## measurements for which there is BOTH a mean and standard deviation.
+  ##
+  ## Args:
+  ##   directory: The root directory of where the raw HAR data set resides. 
+  ##
+  ## Returns:
+  ##   A data frame of two variables. The first column is the column number of 
+  ##   either a mean or standard deviation measurement from the full 561-feature 
+  ##   vector and the second column is the measurement variable's name.
+  
   ## read in the list of all the possible features/measurements in the 561-feature vector
   measurements <- read.table(file.path(directory,"features.txt"), stringsAsFactors=F)
   names(measurements) <- c("Col.Number", "Feature")
@@ -167,14 +279,19 @@ MeasurementsOfInterest <- function(directory = "UCI HAR Dataset") {
   measurements
 }
 
-ActivityLabelMapping <- function(directory = "UCI HAR Dataset") {
-  ## read in the list of activity labels
-  activity.labels <- read.table(file.path(directory,"activity_labels.txt"), stringsAsFactors=F)
-  names(activity.labels) <- c("ActivityNumber", "ActivityLabel")
-  activity.labels
-}
-
 AddSubjectIdentifers <- function(data, test.or.train = "test", directory = "UCI HAR Dataset") {
+  ## Adds subject identifiers to either a test or train set of measurement observations.
+  ##
+  ## Args:
+  ##   data: a data frame of measurement observations
+  ##   test.or.train: A character variable that indicates which measurement data set to 
+  ##                  add subject identifiers to (the test set or the train set)
+  ##   directory: The root directory of where the raw HAR data set resides. 
+  ##
+  ## Returns:
+  ##   A new data frame with the column of subject identifiers PREPENDED to the
+  ##   measurement observations. 
+
   if (!(test.or.train %in% c("test", "train"))) {
     stop("invalid data subset requested; it must be either 'test' or 'train'")
   }
@@ -190,6 +307,18 @@ AddSubjectIdentifers <- function(data, test.or.train = "test", directory = "UCI 
 }
 
 AddActivityLabels <- function(data, test.or.train = "test", directory = "UCI HAR Dataset") {
+  ## Adds activity labels to either a test or train set of measurement observations.
+  ##
+  ## Args:
+  ##   data: a data frame of measurement observations
+  ##   test.or.train: A character variable that indicates which measurement data set to 
+  ##                  add subject identifiers to (the test set or the train set)
+  ##   directory: The root directory of where the raw HAR data set resides. 
+  ##
+  ## Returns:
+  ##   A new data frame with the column of (meaningful) activity labels APPENDED to the
+  ##   measurement observations. 
+  
   if (!(test.or.train %in% c("test", "train"))) {
     stop("invalid data subset requested; it must be either 'test' or 'train'")
   }
@@ -204,4 +333,21 @@ AddActivityLabels <- function(data, test.or.train = "test", directory = "UCI HAR
   activity.labels.mapping <- ActivityLabelMapping(directory)  
   ActivityLabel <- activity.labels.mapping[activity.df[, 1],2]
   cbind(data, ActivityLabel)
+}
+
+ActivityLabelMapping <- function(directory = "UCI HAR Dataset") {
+  ## Reads in the activity labels associated with the activity numbers in the
+  ## HAR data set (from the activity_labels.txt file).
+  ##
+  ## Args:
+  ##   directory: The root directory of where the raw HAR data set resides. 
+  ##
+  ## Returns:
+  ##   A data frame of two variables. The first column is the activity number and
+  ##   the second column is the activity name/label (e.g. 1 = WALKING)
+  
+  ## read in the list of activity labels
+  activity.labels <- read.table(file.path(directory,"activity_labels.txt"), stringsAsFactors=F)
+  names(activity.labels) <- c("ActivityNumber", "ActivityLabel")
+  activity.labels
 }
